430a5ce24802561a33151cc021bf0d72f5854807
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2024-09-02 09:48:48 -0400

    Update ed.ps1

--- ed.ps1
+++ ed.ps1
@@ -1,5 +1,5 @@
 $PathToFile = "D:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Log\edu.exe"
-$Interval = 600 # ÇáæÞÊ ÈÇáËæÇäí Èíä ßá ÊÔÛíá
+$Interval = 20 # ÇáæÞÊ ÈÇáËæÇäí Èíä ßá ÊÔÛíá
 
 while ($true) {
     Start-Process -FilePath $PathToFile
